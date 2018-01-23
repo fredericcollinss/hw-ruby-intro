f99c961174ab039a83073a8f33a6a500f4775021
Tri Pham <fredericcollinss@gmail.com>
2018-01-23 09:21:24 -0600

    Complete part 1'

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,16 +2,37 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum (arr)
+  sum = 0
+  arr.each do |elem|
+    sum += elem
+  end
+  sum
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum (arr)
+  if arr.size == 0
+    return 0
+  elsif arr.size == 1
+    return arr[0]
+  end
+
+  # sort and return sum of the last 2 elem
+  temp_array = arr.sort
+  temp_array[temp_array.size - 1] + temp_array[temp_array.size - 2]
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n? (arr, n)
+  arr.each_with_index do |item, index|
+    j = index + 1
+    while j < arr.length
+      if  item + arr[j] == n
+        return true
+      end
+      j += 1
+    end
+  end
+  false
 end
 
 # Part 2
